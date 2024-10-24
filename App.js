36cda52242ded5848951f9eca1ce0db1b7cf002e
Your Name <you@example.com>
2024-10-24 20:10:51 +0300

    finised Register and Login Screen

--- App.js
+++ App.js
@@ -3,13 +3,15 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { NavigationContainer } from "@react-navigation/native";
-import Login from "./src/screens/Login";
+
 import Profile from "./src/screens/Profile";
 const queryClient = new QueryClient();
 
 import { TextInput } from "react-native";
 import WelcomeButton from "./src/components/WelcomeButton";
 import PetIdBlock from "./src/components/PetIdBlock";
+import Register from "./src/screens/Register";
+import Login from "./src/screens/Login";
 
 const Stack = createNativeStackNavigator();
 export default function App() {
@@ -18,7 +20,12 @@ export default function App() {
       <NavigationContainer>
         <Stack.Navigator>
           {/* <Stack.Screen name="pet Id Blok" component={PetIdBlock} /> */}
-          <Stack.Screen name="Profile" component={Profile} />
+          {/* <Stack.Screen name="Profile" component={Profile} /> */}
+          <Stack.Screen
+            name="Login"
+            component={Login}
+            options={{ headerShown: false }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </QueryClientProvider>
--- src/components/WelcomeButton.js
+++ src/components/WelcomeButton.js
@@ -3,9 +3,10 @@ import React from 'react'
 
 const WelcomeButton = ({text, color, width, height}) => {
   return (
-    <TouchableOpacity style={{backgroundColor:color, width:width, height:height, borderRadius:20, alignItems:"center", justifyContent:"center"}}><Text style={{color: "white"}}>{text}</Text></TouchableOpacity>
+    <TouchableOpacity style={{backgroundColor:color, width:width, height:height, borderRadius:30, alignItems:"center", justifyContent:"center"}}><Text style={{color: "white"}}>{text}</Text></TouchableOpacity>
   )
 }
+
 
 
 
