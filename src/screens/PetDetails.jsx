3e922aa255d5b5db2bb51e4b7cccdbb5217ed9b5
Hussain Alhaddad <hussain.alhaddad777@gmail.com>
2024-10-28 08:58:41 +0300

    add secreens

---
+++ src/screens/PetDetails.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const PetDetails = () => {
+  return (
+    <View>
+      <Text>PetDetails</Text>
+    </View>
+  )
+}
+
+export default PetDetails
